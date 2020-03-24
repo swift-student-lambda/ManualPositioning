498aefa214f727ed52f871bdb0add54e918dac02
Shawn Gee <shawngee@gmail.com>
2020-03-24 09:28:57 -0400

    Initial Commit

---
+++ ManualPositioning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManualPositioning
+//
+//  Created by Shawn Gee on 3/24/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
